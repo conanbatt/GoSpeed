fe820295e65687c7fd313561396becd7b84b506a
Patricio Reboratti <darthpolly@gmail.com>
2011-09-16 17:04:24 -0300

    Expanded and corrected the test suites. Much more work TODO here...

--- test/js/board-init.js
+++ test/js/board-init.js
@@ -1,37 +1,162 @@
 
-var BOARD_SIZE = 19;
-var BOARD_RULESET = "Japanese";
-var BOARD_MODE = "play";
+// ---------------- //
+// Board Init Empty //
+// ---------------- //
 
-module("Board Init", {
+module("Board Init Empty", {
+	setup: function() {
+		this.gospeed = new GoSpeed();
+	}
+});
+
+test("Size", function() {
+	equal(this.gospeed.size, 19, "When not specified, board size should default to 19.");
+});
+
+test("Mode", function() {
+	equal(this.gospeed.mode, "play", "When not specified, game mode should default to 'play'.");
+});
+
+test("Ruleset", function() {
+	equal(this.gospeed.ruleset, "Japanese", "When not specified, ruleset should default to 'Japanese'.");
+});
+
+test("Next move", function() {
+	equal(this.gospeed.next_move, "B", "The first move should belong to 'B' player.");
+});
+
+test("Ko", function() {
+	equal(this.gospeed.ko, undefined, "At first, Ko should be undefined.");
+});
+
+test("Grid", function() {
+	equal(this.gospeed.grid.length, this.gospeed.size, "The grid's row count should be equal to it's size value.");
+	var count = this.gospeed.size;
+	for (var i = 0 ; i < count ; i++) {
+		equal(this.gospeed.grid[i].length, this.gospeed.size, "The grid's column count for row[" + i + "] should be equal to board's size value.");
+	}
+});
+
+test("Shower", function() {
+	equal(this.gospeed.sgf, undefined, "And so should be the 'sgf' property, wich would hold the original content of such div.");
+	equal(this.gospeed.shower, undefined, "If there's no div, there should be no showing class.");
+});
+
+test("GameTree", function() {
+	equal(this.gospeed.game_tree.root.play, null, "At first, the game tree should have a root node with no play in it.");
+	equal(this.gospeed.game_tree.root.prev, null, "The root node must never have a previous node.");
+	equal(this.gospeed.game_tree.root.next.length, 0, "There must not be any nodes next to the root.");
+	equal(this.gospeed.game_tree.root.last_next, null, "No last_next either");
+	equal(this.gospeed.game_tree.actual_move, this.gospeed.game_tree.root, "Actual move should be the root node, ie no move.");
+	equal(this.tree_div, undefined, "The div which holds the graphic representation of the game tree should not be defined.");
+});
+
+test("Online", function() {
+	equal(this.gospeed.my_colour, undefined, "Without argument, online colour is undefined.");
+	equal(this.gospeed.turn_count, 0, "No moves played, so turn_count is 0.");
+});
+
+test("Paths", function() {
+	equal(this.gospeed.server_path_game_move, undefined, "By default, there is no server nor move_path.");
+	equal(this.gospeed.server_path_gospeed_root, undefined, "By default, there is no server nor gospeed_root.");
+});
+
+
+// --------------- //
+// Board Init Full //
+// --------------- //
+
+var BOARD_SIZE = 13;
+var BOARD_RULESET = "Chinese";
+var BOARD_MODE = "play_online";
+var BOARD_DIV_ID = "gospeed_board";
+var BOARD_TREE_DIV_ID = "gametree_div";
+var BOARD_SHOWER = "graphic";
+var BOARD_MY_COLOUR = "B";
+var BOARD_SERVER_PATH_GAME_MOVE = "21/play";
+var BOARD_SERVER_PATH_GOSPEED_ROOT = "/resources/gospeed";
+var BOARD_DIV_ORIGINAL_CONTENT = "(;FF[4];B[bc];W[ff];B[fb];W[bf])";
+
+module("Board Init Full", {
 	setup: function() {
 		var conf = {
 			size: BOARD_SIZE,
 			ruleset: BOARD_RULESET,
-			mode: BOARD_MODE
+			mode: BOARD_MODE,
+			div_id: BOARD_DIV_ID,
+			tree_div_id: BOARD_TREE_DIV_ID,
+			shower: BOARD_SHOWER,
+			my_colour: BOARD_MY_COLOUR,
+			server_path_game_move: BOARD_SERVER_PATH_GAME_MOVE,
+			server_path_gospeed_root: BOARD_SERVER_PATH_GOSPEED_ROOT
 		};
+		document.getElementById(BOARD_DIV_ID).innerHTML = BOARD_DIV_ORIGINAL_CONTENT;
 		this.gospeed = new GoSpeed(conf);
+	},
+	teardown: function() {
+		delete this.gospeed;
 	}
 });
 
-test("Board Size and Grid", function() {
-	equal(this.gospeed.size, BOARD_SIZE);
-	equal(this.gospeed.grid.length, this.gospeed.size);
-	var count = this.gospeed.size;
-	for (var i = 0 ; i < count ; i++) {
-		equal(this.gospeed.grid[i].length, this.gospeed.size);
-	}
+test("Size", function() {
+	equal(this.gospeed.size, BOARD_SIZE, "As configured, board size should be " + BOARD_SIZE + ".");
+});
+
+test("Mode", function() {
+	equal(this.gospeed.mode, BOARD_MODE, "As configured, board mode should be " + BOARD_MODE + ".");
 });
 
 test("Ruleset", function() {
-	equal(this.gospeed.ruleset, BOARD_RULESET);
-});
-
-test("Board Mode", function() {
-	equal(this.gospeed.mode, BOARD_MODE);
+	equal(this.gospeed.ruleset, BOARD_RULESET, "As configured, ruleset should be " + BOARD_RULESET + ".");
 });
 
 test("Next move", function() {
-	equal(this.gospeed.next_move, "B");
+	equal(this.gospeed.next_move, "B", "The first move should belong to 'B' player.");
+});
+
+test("Ko", function() {
+	equal(this.gospeed.ko, undefined, "At first, Ko should be undefined.");
+});
+
+test("Grid", function() {
+	equal(this.gospeed.grid.length, this.gospeed.size, "The grid's row count should be equal to it's size value.");
+	var count = this.gospeed.size;
+	for (var i = 0 ; i < count ; i++) {
+		equal(this.gospeed.grid[i].length, this.gospeed.size, "The grid's column count for row[" + i + "] should be equal to board's size value.");
+	}
+});
+
+test("DivContent", function() {
+	ok(this.gospeed.sgf == undefined || this.gospeed.sgf == BOARD_DIV_ORIGINAL_CONTENT, "OK, we have a div. If the div was not empty at startup, the board should have saved it's content into de sgf property.");
+});
+
+if (BOARD_SHOWER == "graphic") {
+	test("GoGraphic", function() {
+		ok(this.gospeed.shower instanceof GoGraphic, "As configured, the showing engine should be GoGraphic.");
+		ok(this.gospeed.shower.game === this.gospeed, "Game reference in GoGraphic is correct.");
+		equal(this.gospeed.shower.div, document.getElementById(BOARD_DIV_ID), "The shower div property must hold the result of document.getElementById(BOARD_DIV_ID).");
+		// TODO: complete gograhpic tests.
+	});
+}
+
+test("GameTree", function() {
+	equal(this.gospeed.game_tree.root.play, null, "At first, the game tree should have a root node with no play in it.");
+	equal(this.gospeed.game_tree.root.prev, null, "The root node must never have a previous node.");
+	equal(this.gospeed.game_tree.root.next.length, 0, "There must not be any nodes next to the root.");
+	equal(this.gospeed.game_tree.root.last_next, null, "No last_next either");
+	equal(this.gospeed.game_tree.actual_move, this.gospeed.game_tree.root, "Actual move should be the root node, ie no move.");
+	equal(this.gospeed.tree_div, document.getElementById(BOARD_TREE_DIV_ID), "The div which holds the graphic representation of the game tree should be the result of document.getElementById() with the tree_div_id argument.");
+});
+
+test("Online", function() {
+	equal(this.gospeed.my_colour, BOARD_MY_COLOUR, "As configured, my_colour is '" + BOARD_MY_COLOUR + "'.");
+	equal(this.gospeed.turn_count, 0, "No moves played, so turn_count is 0.");
+});
+
+test("Paths", function() {
+	//var re_path = /^\/?([^\/]+\/)+$/;
+	var re_path = /.*\/$/; // Ends with slash char '/'
+	ok(this.gospeed.server_path_game_move == undefined || this.gospeed.server_path_game_move != "", "Whether the server_path_game_move is undefined or it is a not empty string.");
+	ok(this.gospeed.server_path_gospeed_root == undefined || re_path.test(this.gospeed.server_path_gospeed_root), "Whether the server_path_gospeed_root is undefined or it is a string which ends in slash ('/').");
 });
 
--- test/js/board-play.js
+++ test/js/board-play.js
@@ -15,29 +15,39 @@ module("Board Play", {
 });
 
 test("put_stone: raw stone-placing method", function() {
-	equal(this.gospeed.grid[0][0], undefined);
+	equal(this.gospeed.grid[0][0], undefined, "At first, position (0, 0) of the grid should be 'undefined'.");
 	this.gospeed.put_stone("W", 0, 0);
-	equal(this.gospeed.grid[0][0], "W");
+	equal(this.gospeed.grid[0][0], "W", "Once we put_stone('W', 0, 0), grid[0][0] should be 'W'.");
 
-	equal(this.gospeed.grid[1][1], undefined);
+	equal(this.gospeed.grid[1][1], undefined, "This far, position (1, 1) of the grid should be 'undefined'.");
 	this.gospeed.put_stone("B", 1, 1);
-	equal(this.gospeed.grid[1][1], "B");
+	equal(this.gospeed.grid[1][1], "B", "Once we put_stone('B', 1, 1), grid[1][1] should be 'B'.");
 });
 
 test("get_pos: raw position-checking method", function() {
-	equal(this.gospeed.get_pos(2, 2), undefined);
-	this.gospeed.put_stone("W", 2, 2);
-	equal(this.gospeed.get_pos(2, 2), "W");
+	equal(this.gospeed.get_pos(2, 2), undefined, "At first, position (2, 2) of the grid should be 'undefined'.");
+	this.gospeed.grid[2][2] = "W";
+	equal(this.gospeed.get_pos(2, 2), "W", "Once we did grid[2][2] = 'W', get_pos(2, 2) should be 'W'.");
 
-	equal(this.gospeed.get_pos(3, 3), undefined);
+	equal(this.gospeed.get_pos(3, 3), undefined, "This far, position (3, 3) of the grid should be 'undefined'.");
+	this.gospeed.grid[3][3] = "B";
+	equal(this.gospeed.get_pos(3, 3), "B", "Once we did grid[3][3] = 'B', get_pos(3, 3) should be 'B'.");
+});
+
+test("put_stone - get_pos: combined stone-placing / position-checking method", function() {
+	equal(this.gospeed.get_pos(2, 2), undefined, "At first, get_pos(2, 2) should be 'undefined'.");
+	this.gospeed.put_stone("W", 2, 2);
+	equal(this.gospeed.get_pos(2, 2), "W", "Once we put_stone('W', 2, 2), get_pos(2, 2) should be 'W'.");
+
+	equal(this.gospeed.get_pos(3, 3), undefined, "This far, get_pos(3, 3) should be 'undefined'.");
 	this.gospeed.put_stone("B", 3, 3);
-	equal(this.gospeed.get_pos(3, 3), "B");
+	equal(this.gospeed.get_pos(3, 3), "B", "Once we put_stone('B', 3, 3), get_pos(3, 3) should be 'B'.");
 });
 
 test("play: in-game stone-placing method", function() {
-	equal(this.gospeed.next_move, "B");
+	equal(this.gospeed.next_move, "B", "At the begining, next_move should be 'B'.");
 	this.gospeed.play(4, 4);
-	equal(this.gospeed.get_pos(4, 4), "B");
-	equal(this.gospeed.next_move, "W");
+	equal(this.gospeed.get_pos(4, 4), "B", "After we play in (4, 4) the stone in that position should be 'B'.");
+	equal(this.gospeed.next_move, "W", "After the first move, next_move should be 'W'.");
 });
 

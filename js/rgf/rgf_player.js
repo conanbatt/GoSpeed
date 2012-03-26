05143009dab7b5bfc4f665e5404fb68bbe2be09d
Patricio Reboratti <darthpolly@gmail.com>
2012-03-26 16:18:45 -0300

    Added callback to RGF when board is clicked. XXX: horrible gographics.js code insertion.

--- js/rgf/rgf_player.js
+++ js/rgf/rgf_player.js
@@ -1,5 +1,6 @@
 
 function RGFPlayer(config) {
+	var self = this;
 	// Customize configuration
 		var custom_config = {
 			mode: "play",
@@ -17,11 +18,15 @@ function RGFPlayer(config) {
 
 	// Initialize GoSpeed with custom config
 		this.gospeed = new GoSpeed(custom_config);
-		this.gospeed.disconnect();
+		this.gospeed.mode = "rgf";
+		this.gospeed.callbacks["rgf_board_click"] = function() {
+			self.boardClicked.call(self, arguments[0], arguments[1])
+		};
 
 	// Private props
 		this.new_node = false;
-}
+		this.recording = false;
+};
 
 RGFPlayer.prototype.createNode = function() {
 	if (!this.gospeed.game_tree.actual_move.root) {
@@ -37,7 +42,8 @@ RGFPlayer.prototype.addProperty = function(name, arg) {
 		case "W":
 			// Check if turn corresponds with color
 			if (name == this.gospeed.get_next_move()) {
-				this.gospeed.connect();
+				var store_mode = this.gospeed.mode;
+				this.gospeed.mode = "play";
 				if (arg == "") {
 					// Pass
 					bRes = this.gospeed.pass();
@@ -46,7 +52,7 @@ RGFPlayer.prototype.addProperty = function(name, arg) {
 					pos = this.gospeed.sgf_coord_to_pos(arg)
 					bRes = this.gospeed.play(pos.row, pos.col);
 				}
-				this.gospeed.disconnect();
+				this.gospeed.mode = store_mode;
 				this.new_node = false;
 			} else {
 				throw new Error("Not " + name + "'s turn...");
@@ -79,4 +85,29 @@ RGFPlayer.prototype.getCurrentPath = function() {
 
 RGFPlayer.prototype.goTo = function(path) {
 	return this.gospeed.goto_path(path);
+};
+
+RGFPlayer.prototype.boardClicked = function(row, col) {
+	// Do some stuff related with GameStream
+	// return false if it fails,
+	// or run the following code to draw the stone
+	var name = this.gospeed.get_next_move();
+	var arg = this.gospeed.pos_to_sgf_coord(row, col);
+	this.addProperty(name, arg);
+};
+
+RGFPlayer.prototype.enableRecording = function() {
+	this.gospeed.mode = "play";
+	this.recording = true;
+	return true;
+};
+
+RGFPlayer.prototype.disableRecording = function() {
+	this.gospeed.mode = "rgf";
+	this.recording = false;
+	return true;
+};
+
+RGFPlayer.prototype.isRecording = function() {
+	return this.recording;
 };
